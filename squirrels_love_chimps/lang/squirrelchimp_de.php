2f8c60bf18ef69d77fef9a512fd7f6fbb55314ca
Rainer Müller <abelass@mychacra>
2011-11-16 14:28:01 +0100

    Traductions en allemand

---
+++ squirrels_love_chimps/lang/squirrelchimp_de.php
@@ -0,0 +1,27 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	//A
+	'api_errorcode' => 'Zurückmeldung MailChimp. Code:',
+
+	//C
+	'config_erreur' => 'Irrtum : die MailCimp Zugansdaten sind nicht gespeichert, Der Site Administrator kann dies  : <a href="ecrire/?exec=configurer_mailchimp">hier korrigieren</a>',
+	'config_squirrel_chimp' => 'Generell',
+	'configurer_erreur_api' => 'Irrtum, Ihre Daten wurden nicht gespeichert',
+	'configurer_legend' => 'MailChimp Identifizierungs Parameter',
+	'configurer_apikey' => 'Einmaliges Kennzeichen des MailChimp Kontos',
+	'configurer_apikey_explication' => 'API Key den man im Verwaltungspanel : <a href="http://admin.mailchimp.com/account/api">MailChimp API</a> findet',
+
+	//R
+
+	'titre_cadre_raccourcis' => 'Abkürzung',
+	'retour_accueil' => 'Zurück zur Hauptseite',
+	'retour_test_api' => 'Verbindung mit MailChimp wurde erstellt, die Daten wurden gespeichert',
+	
+	//T
+	'titre_plugin' => 'Squirrels Love Chimps',
+);
+
+?>
---
+++ squirrels_love_chimps_campaigns/lang/scc_de.php
@@ -0,0 +1,49 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	//A
+	'activer' => 'Aktivieren',
+	'article_campagne' => 'Wird automatisch bei der Veröffentlichung eines Artikels erstellt.',
+	'article_entier' => 'Den vollständigen Artikel einsehen.',
+	
+	
+	//C
+	'campagnes_articles' => 'Kampagnenherstellung aus einem Artikel',
+	'campagne_creation_unique' => 'Eine Kampagne nur einmal erstellen',
+	'campagnes_mailchimp' => 'Von Spip verwaltete MailChimp Kampagnen',
+	'choisir_liste' => 'Si müssen zuerst eine <a href="@url@">Liste auswählen </a>',
+	'config_squirrel_chimp_campaigns' => 'Kampagnen',
+
+	//G
+	'gestion_campagnes' => 'Kampagnenverwaltung',
+	
+	//E
+	'emails_envoyes' => 'Email wurde versandt',
+	'envoyer_campagne_directe' => 'Die Kampagne direkt nach Veröffenliching des Artikles versenden?',
+	'"Unable to Pull list of Campaign!"' => 'Die Kampagnen Liste ist nicht verfügbar',
+	'explication_article_campagne' => 'Für jede ausgewäḧlte Liste wird eine Kampagne erstellt',
+	'explication_envoyer_campagne_directe' => 'Falls deaktiviert wird die Kampagne als Entwurf gespeichert.',
+	'explication_campagne_creation_unique' => 'Falls deaktiviert, wird bei jeder Aktualisierung des Artikels eine Kampagne erstellt.',
+	'explication_rubrique_campagne' => 'Die Artikel der volgenden Rubriken werden bei der automatischen Erstellung von Kampagnen benutzt. (falls nichts ausgewählt werden alle Artikel berücksichtigt)',
+	
+	
+	//I
+	'id_campagne' => 'Kampagne Id',	
+
+	//M
+	'modele_liste_campagnes' => 'Eine Kampagnenliste anzeigen',	
+
+	//N
+	'nom_campagne' => 'Name',	
+	
+	//R
+	'rubrique_campagne'=>'Rubriken zur Kampagnenerstellung',
+	
+	//S
+	'statut_campagne'=>'Status',
+
+);
+
+?>
