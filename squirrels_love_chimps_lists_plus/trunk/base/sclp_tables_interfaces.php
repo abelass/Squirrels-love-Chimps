e53188c47c3adc57380e41aeeb9b610d7840fa37
Rainer Müller <rainer@websimple.be>
2013-05-20 11:21:53 +0200

    declarer les tables dans interfaces

--- squirrels_love_chimps_lists_plus/trunk/base/sclp_tables_interfaces.php
+++ squirrels_love_chimps_lists_plus/trunk/base/sclp_tables_interfaces.php
@@ -2,6 +2,8 @@
 
 function sclp_declarer_tables_interfaces($tables_interfaces){
 	$tables_interfaces['table_des_tables']['listes'] = 'listes';
+    $tables_interfaces['table_des_tables']['auteurs_listes'] = 'auteurs_listes';    
+    $tables_interfaces['table_des_tables']['listes_syncro'] = 'listes_syncro';      
 	
 	return $tables_interfaces;
 }
