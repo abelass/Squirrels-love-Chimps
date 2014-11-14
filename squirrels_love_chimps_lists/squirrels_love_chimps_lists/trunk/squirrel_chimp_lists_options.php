71cce21dc9ffd9973d73d932a4a320e4665e6f79
unknown <mullrai@D02DI1414784BUD.net1.cec.eu.int>
2014-11-14 10:00:35 +0100

    nettoyage

--- squirrels_love_chimps_lists/squirrels_love_chimps_lists/trunk/squirrel_chimp_lists_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
-// Declaration de la pipeline pour le traitement du formulaire de configuration
-$GLOBALS['spip_pipeline']['squirrel_chimp_lists_config_traiter'];
-
-
-?>
